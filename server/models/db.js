92b88261b06b69cad05184bebf5f4c99d791764c
Kenzo Wong <kenzowong86@gmail.com>
2019-05-17 01:52:46 -0400

    meow

--- server/models/db.js
+++ server/models/db.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 
-// const databaseUrl = 'postgres://localhost:5432/tripplanner';
-const databaseUrl =
-  'postgres://djkocjbqmjhrww:dd4d10fa4e2cdae5f1aaf9a7802d068e64d3c151b1a7a2a1ccfe4ac376590182@ec2-75-101-147-226.compute-1.amazonaws.com:5432/df4tkcsr4pc34b';
+const databaseUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/tripplanner';
 
 const db = new Sequelize(databaseUrl, {
   logging: false,
