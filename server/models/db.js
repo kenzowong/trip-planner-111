90c3dd1cdaa62f0ccc6be98018b50fb83f2709dd
Kenzo Wong <kenzowong86@gmail.com>
2019-05-17 03:41:25 -0400

    meow

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
-const databaseUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/tripplanner';
+const databaseUrl =
+  process.env.DATABASE_URL || 'postgres://localhost:5432/tripplanner';
 
 const db = new Sequelize(databaseUrl, {
   logging: false,
